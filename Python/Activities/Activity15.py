447898601d1013a44c2039b7731b217aa615760b
Sakeena <sakeena.alamuru786@gmail.com>
2026-01-09 21:54:04 +0530

    Add files via upload

---
+++ Python/Activities/Activity15.py
@@ -0,0 +1,5 @@
+x = None
+try:
+    print(x)
+except NameError:
+    print("x hasn't been defined yet.")
---
+++ Python/Activities/pandas_csv.py
@@ -0,0 +1,16 @@
+	
+
+import pandas as pd
+ 
+
+data = {
+  "X": [1, 2, 3, 4, 5],
+  "Y": [5, 4, 3, 2, 1],
+  "Z": [3, 2, 1, 4, 5]
+}
+ 
+
+df = pd.DataFrame(data)
+ 
+# Print the DataFrame
+print(df)
